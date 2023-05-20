1e619fe8eb636b8e90c4a3d62db81fedcd36e869
kfrFahim <kfrfahim3@gmail.com>
2023-05-20 10:18:05 +0600

    delete

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // Mongodb
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6eoz53h.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -39,7 +39,17 @@ async function run() {
 
 
 
+    app.get("/searchtoy/:text" , async(req ,res)=>{
+      const searchToy = req.params.text;
 
+      const result = await toysCollection.find({
+        $or : [
+          {name: {$regex: searchToy, $options: "i"}}
+        ],
+      }).toArray();
+
+      res.send(result)
+    });
 
 
 
@@ -67,6 +77,13 @@ async function run() {
       console.log(req.params.email);
       const result = await toysCollection.find({postedBy : req.params.email}).toArray();
       res.send(result);
+  })
+
+  app.delete("/mytoys/:id" , async(req ,res) => {
+    const id = req.params.id;
+    const query = {_id : new ObjectId(id)}
+    const result = await toysCollection.deleteOne(query);
+    res.send(query)
   })
 
 
